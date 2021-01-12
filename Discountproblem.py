e8b3f0ec70761d81240cc26972e2523f71d92556
Rhythm <76173406+Rhythm001@users.noreply.github.com>
2021-01-12 15:24:06 +0530

    Add files via upload

---
+++ Discountproblem.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 12:07:21 2021
+
+@author: PC
+"""
+
+quantity=int(input("Enter the quantity(in units) here: "))
+costperunit=int(input("Enter the cost of 1 unit: "))
+
+cost=quantity*costperunit
+
+discount=(quantity*costperunit)/10 #since it's cost*(10/100)
+final=cost-discount
+
+if(cost>1000):
+    print("Congrats! You got a discount of %d. You just have to pay %u." %(discount, final))
+else:
+    print("You have to pay: %f" %cost)
